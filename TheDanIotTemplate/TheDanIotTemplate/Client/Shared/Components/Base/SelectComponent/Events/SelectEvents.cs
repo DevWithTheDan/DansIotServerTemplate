07c6b890040e12ea51fa3613102f28bb6e42acef
dan <67156578+DevWithTheDan@users.noreply.github.com>
2023-05-20 17:36:52 +0100

    new select component

---
+++ TheDanIotTemplate/TheDanIotTemplate/Client/Shared/Components/Base/SelectComponent/Events/SelectEvents.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TheDanIotTemplate.Client.Shared.Components.Base.SelectComponent.Events
+{
+    public static class SelectEvents
+    {
+        public static EventHandler? OnChange;
+        public static string? LastSelectedId;
+        public static string? LastCallbackReference;
+
+        public static void Invoke(object? sender, ChangeEventArgs e, string callbackReference)
+        {
+            if (e.Value != null)
+            {
+                LastSelectedId = e.Value.ToString();
+                LastCallbackReference = callbackReference;
+                OnChange?.Invoke(sender, e);
+            }
+        }
+    }
+}
---
+++ TheDanIotTemplate/TheDanIotTemplate/Client/Shared/Components/Base/SelectComponent/Objects/StandardSelect.cs
@@ -0,0 +1,8 @@
+﻿namespace TheDanIotTemplate.Client.Shared.Components.Base.SelectComponent.Objects
+{
+    public class StandardSelect
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
